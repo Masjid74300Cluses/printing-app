83cf31a873dd4fdf7e852ba4b794b6ab1e4b924c
krimo <kissab@gmail.com>
2023-07-09 08:48:23 +0200

    update constant

--- constants/constants.js
+++ constants/constants.js
@@ -1,3 +1,4 @@
 export const TCP_INTERFACE='tcp://192.168.1.17';
 export const MAIN_EMAIL='clusesmosquee@gmail.com';
 export const MAIN_PASS='npkjwpdowyetamff';
+export const URL_NGROK='https://db8e-213-56-216-19.eu.ngrok.io'
